ac373bd28ae5fe9075d90cb64ccbeb4a44dc7b7f
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-07 22:33:34 +0000

    day 7

---
+++ 2022/day7/part1.ts
@@ -0,0 +1,31 @@
+type Directory = {
+  name: string;
+  parent: Directory | undefined;
+  files: Array<{ name: string; size: number }>;
+  directories: Array<Directory>;
+};
+
+const parse = (root: Directory | undefined) =>
+  Deno.readTextFileSync("./input.txt").split("\n").reduce((node, line, i) => {
+    const [col1, col2, col3] = line.split(" ");
+    if (col1 === "$" && col2 === "cd") {
+      if (col3 === "/") return root;
+      if (col3 === "..") return node?.parent;
+      return node?.directories.find((d) => d.name === col3) ?? node;
+    }
+    if (col1 === "dir")
+      node?.directories.push({ name: col2, parent: node, files: [], directories: []});
+    if (!Number.isNaN(Number(col1)) && line)
+      node?.files.push({ name: col2, size: Number(col1) });
+    return node;
+  }, root) && root;
+
+const sumDirs = (node: Directory, answer) => {
+    const fileSizes = node.files.reduce((sum, f) => sum + f.size, 0);
+    const dirSizes = node.directories.reduce((sum, d) => sum + sumDirs(d, answer)[0], fileSizes);
+    if (dirSizes <= 100000) answer.push({ name: node.name, size: dirSizes });
+    return [dirSizes, answer];
+}
+
+const root = parse({ name: "/", parent: undefined, files: [], directories: [] });
+console.log(sumDirs(root, [])[1].reduce((sum, dir) => sum + dir.size, 0));
