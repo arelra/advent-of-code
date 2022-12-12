b63a48da66734f845467dc6e07ac422b7bbbd0b8
Ravi <7014230+arelra@users.noreply.github.com>
2022-12-12 01:16:06 +0000

    day 11

---
+++ 2022/day11/part1.ts
@@ -0,0 +1,41 @@
+type Monkey = {
+    items: number[],
+    inspect: (item: number) => number,
+    throwTo: (item: number) => number,
+    inspected: number,
+};
+
+const parse = (paragraphs): Array<Monkey> =>
+    paragraphs.reduce((monkeys, paragraph) => {
+        const lines = paragraph.split('\n')
+        const items = lines[1].substr(18).split(',').map(n => Number(n.trim(n)));
+        const inspect = { op: lines[2].substr(23, 1), val: lines[2].substr(25)};
+        const throwTo = { div: lines[3].substr(21), t: lines[4].substr(29), f: lines[5].substr(30)};
+        const monkey = {
+            items,
+            inspect: inspect.op === '+' ? (i) => i + Number(inspect.val) : (i) => i * (inspect.val === 'old' ? i : Number(inspect.val)),
+            throwTo: (w) => w % throwTo.div === 0 ? throwTo.t : throwTo.f,
+            inspected: 0,
+        }
+        return monkeys.push(monkey) && monkeys;
+    }, []);
+
+const game = (monkeys: Array<Monkey>, rounds: number) => {
+    for (let r = 0; r < rounds; r++)
+        for (let m = 0; m < monkeys.length; m++) {
+            const monkey = monkeys[m];
+            const { items, inspect, throwTo } = monkey;
+            for (let i = 0; i < items.length; i++) {
+                const item = Math.floor(inspect(items[i]) / 3);
+                monkeys[throwTo(item)].items.push(item);
+                monkey.inspected++;
+            }
+            monkey.items = [];
+        }
+    return monkeys;
+}
+
+console.log(game(parse(Deno.readTextFileSync("./input.txt").split("\n\n")), 20)
+    .sort((a,b) => b.inspected - a.inspected)
+    .slice(0,2)
+    .reduce((res, i) => res * i.inspected, 1));
